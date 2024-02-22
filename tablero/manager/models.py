7c6cb364d11d789841a2fd855e72987cb74a4b2e
DiegoAndresGlez <diegoglez077@gmail.com>
2024-02-22 13:53:39 -0800

    added models

--- tablero/manager/models.py
+++ tablero/manager/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class ToDoList(models.Model):
+class ToDoTask(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     status_done = models.BooleanField(default=False)
